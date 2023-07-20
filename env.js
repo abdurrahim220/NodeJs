0d9b03cd31833fc63faef578f074b9fc728ae3f1
Nadim660 <100480279+Nadim660@users.noreply.github.com>
2023-07-20 18:47:30 +0600

    nothig

---
+++ env.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+console.log(process.env.NAME)
+console.log(process.env.PROFESSION)
+console.log("I am starting a new serries on",process.env.COURSE)
+
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("This is nodejs Tutorial for beginners");
+console.log("This is the first tutorial on nodejs");
+console.log("This is the first tutorial on nodejs");
+console.log("This is the first tutorial on nodejs");
+
+// process.exit(1)
+
+// process.exitCode = 0;
